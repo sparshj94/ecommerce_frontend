31b345ea82a3e1639c15eb26e187d559ed1d3f66
sparshj94 <jsparsh93@gmail.com>
2024-03-31 23:40:04 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy: {
-    //   "/api/v1": {
-    //     target: "http://localhost:3000",
-    //     // changeOrigin: true,
-    //     // rewrite: (path) => path.replace(/^\/api/, "/api"),
-    //   },
-    // },
-  },
+  base: "/ecommerce_frontend/",
 });
+
+// server: {
+//   // proxy: {
+//   //   "/api/v1": {
+//   //     target: "http://localhost:3000",
+//   //     // changeOrigin: true,
+//   //     // rewrite: (path) => path.replace(/^\/api/, "/api"),
+//   //   },
+//   // },
+// },
